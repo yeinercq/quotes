83a265346a9254e6b56b5ab1fa7b7b4cd36ce8ca
YeinerCQ <ycarvajalq@gmail.com>
2022-10-27 15:46:36 -0500

    add turbo stream behavior to real-time updates

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,16 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+  #
+  ## this is the same -> after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  ## this is for asynchronous job
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
